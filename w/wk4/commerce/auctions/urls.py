8650e6cdf4bd474d82db221cf4129756800defbc
saravanankesa <140973037+saravanankesa@users.noreply.github.com>
2024-03-26 17:11:52 +0000

    Tue, Mar 26, 2024, 11:11 AM -06:00

--- w/wk4/commerce/auctions/urls.py
+++ w/wk4/commerce/auctions/urls.py
@@ -4,12 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", auth_views.LoginView.as_view(), name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path('create_listing/', views.create_listing, name='create_listing'),
     path('listing/<int:pk>/', views.listing_detail, name='listing_detail'),
     path('edit_listing/<int:pk>/', views.edit_listing, name='edit_listing'),
     path('delete_listing/<int:pk>/', views.delete_listing, name='delete_listing'),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
 ]
