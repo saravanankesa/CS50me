0ae574ccf2a9bc959a4bb931459480397b03e25a
saravanankesa <140973037+saravanankesa@users.noreply.github.com>
2024-03-26 17:03:59 +0000

    Tue, Mar 26, 2024, 11:03 AM -06:00

--- w/wk4/commerce/auctions/urls.py
+++ w/wk4/commerce/auctions/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
+    path("login", views.auth_views.LoginView.as_view(), name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path('create_listing/', views.create_listing, name='create_listing'),
