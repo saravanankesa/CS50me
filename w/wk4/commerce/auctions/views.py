7cc92e6e5c56fd43bc4efed10772e022738d6491
saravanankesa <140973037+saravanankesa@users.noreply.github.com>
2024-03-26 13:04:38 +0000

    Tue, Mar 26, 2024, 7:04 AM -06:00

--- w/wk4/commerce/auctions/views.py
+++ w/wk4/commerce/auctions/views.py
@@ -3,7 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.forms import ListingForm
+from .forms import ListingForm
 
 from .models import User
 
