a3616407a99ff9eebcb701422556b0fc947e7204
saravanankesa <140973037+saravanankesa@users.noreply.github.com>
2024-03-26 17:41:24 +0000

    Tue, Mar 26, 2024, 11:41 AM -06:00

--- w/wk4/commerce/auctions/urls.py
+++ w/wk4/commerce/auctions/urls.py
@@ -3,12 +3,13 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", auth_views.LoginView.as_view(), name="login"),  # Default route to login page
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path('create_listing/', views.create_listing, name='create_listing'),
     path('listing/<int:pk>/', views.listing_detail, name='listing_detail'),
     path('edit_listing/<int:pk>/', views.edit_listing, name='edit_listing'),
     path('delete_listing/<int:pk>/', views.delete_listing, name='delete_listing'),
-    path("login", auth_views.LoginView.as_view(), name="login"),
+    path("login", auth_views.LoginView.as_view(), name="login"),  # Additional login route
+    path("index", views.index, name="index"),  # Redirect to index after successful login
 ]
