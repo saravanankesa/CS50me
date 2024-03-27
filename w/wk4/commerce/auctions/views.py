04f04e0695dabf47b1514654df4cb91877e32a45
saravanankesa <140973037+saravanankesa@users.noreply.github.com>
2024-03-27 23:21:07 +0000

    Wed, Mar 27, 2024, 5:21 PM -06:00

--- w/wk4/commerce/auctions/views.py
+++ w/wk4/commerce/auctions/views.py
@@ -15,25 +15,6 @@ def index(request):
     active_listings = Listing.objects.filter(created_at__lte=current_datetime)
     return render(request, 'auctions/index.html', {'active_listings': active_listings})
 
-def login_view(request):
-    if request.method == "POST":
-
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "auctions/login.html")
-
 
 def logout_view(request):
     logout(request)
