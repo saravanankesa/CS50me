ba2a2ef88139564686dc29359d7307b4f0031f7b
Saravanan Kesavan <140973037+saravanankesa@users.noreply.github.com>
2024-03-28 19:06:46 +0000

    Thu, Mar 28, 2024, 1:06 PM -06:00

--- w/wk4/commerce/auctions/models.py
+++ w/wk4/commerce/auctions/models.py
@@ -25,7 +25,6 @@ class Listing(models.Model):
     category = models.CharField(max_length=50, blank=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     created_at = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(default=True)
     winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="won_listings")
     is_active = models.BooleanField(default=True)
     highest_bid = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
