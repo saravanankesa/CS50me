e7308cc17a6c94a1fd608fb5176a38801595b6a7
Saravanan Kesavan <140973037+saravanankesa@users.noreply.github.com>
2024-03-29 10:09:23 +0000

    Fri, Mar 29, 2024, 4:09 AM -06:00

--- w/wk4/commerce/commerce/settings.py
+++ w/wk4/commerce/commerce/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/auctions/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
