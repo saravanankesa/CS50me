9eab45a78783d2f83d93baa5de981ce0f15f49a3
Saravanan Kesavan <140973037+saravanankesa@users.noreply.github.com>
2024-03-29 10:15:01 +0000

    Fri, Mar 29, 2024, 4:15 AM -06:00

--- w/wk4/commerce/commerce/settings.py
+++ w/wk4/commerce/commerce/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'auctions/static/auctions/'
 
 
 STATICFILES_DIRS = [
