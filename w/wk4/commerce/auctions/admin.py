4b581b01398061b1fbcbbcde2d3b91e0c24974ac
Saravanan Kesavan <140973037+saravanankesa@users.noreply.github.com>
2024-03-29 10:30:14 +0000

    Fri, Mar 29, 2024, 4:30 AM -06:00

--- w/wk4/commerce/auctions/admin.py
+++ w/wk4/commerce/auctions/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Listing, Comment, Bid
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'starting_bid', 'is_active')
 
-admin.site.register(Listing)
-admin.site.register(Comment)
-admin.site.register(Bid)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('listing', 'user', 'timestamp')
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('listing', 'bidder', 'amount', 'timestamp')
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Bid, BidAdmin)
