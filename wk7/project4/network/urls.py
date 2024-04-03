00413c89604dd158c02afd0742952cd6ffe9652a
Saravanan Kesavan <140973037+saravanankesa@users.noreply.github.com>
2024-04-03 17:56:21 +0000

    Wed, Apr 3, 2024, 11:56 AM -06:00

--- wk7/project4/network/urls.py
+++ wk7/project4/network/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("new_post", views.new_post, name="new_post"),
     path("profile/<str:username>", views.profile, name="profile"),
     path("follow/<str:username>", views.follow, name="follow"),
+    path("following", views.following, name="following"),
 ]
