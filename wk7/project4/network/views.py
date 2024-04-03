2e5ff7f92be38bc5624a204e3b8a5203282abd9d
Saravanan Kesavan <140973037+saravanankesa@users.noreply.github.com>
2024-04-03 18:22:42 +0000

    Wed, Apr 3, 2024, 12:22 PM -06:00

--- wk7/project4/network/views.py
+++ wk7/project4/network/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .forms import NewPostForm
 from .models import User, Post
