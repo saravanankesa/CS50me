ee6f4c414e640d144b671af006a0696e4d6cb176
Saravanan Kesavan <140973037+saravanankesa@users.noreply.github.com>
2024-04-03 18:27:01 +0000

    Wed, Apr 3, 2024, 12:27 PM -06:00

--- wk7/project4/network/views.py
+++ wk7/project4/network/views.py
@@ -44,7 +44,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("login_view"))
+    return HttpResponseRedirect(reverse("login"))
 
 
 def register(request):
