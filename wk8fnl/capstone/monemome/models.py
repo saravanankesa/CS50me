8acfd7442821172330efb81fc458fdb175fb91de
saravanankesa <140973037+saravanankesa@users.noreply.github.com>
2024-04-04 21:38:57 -0600

    Implement database models

---
+++ wk8fnl/capstone/monemome/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+TRANSACTION_TYPES = [
+    ('Expense', 'Expense'),
+    ('Income', 'Income')
+]
+
+
+class Transaction(models.Model):
+    type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    pre_auth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.type}: {self.name} - ${self.amount}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+
+    def __str__(self):
+        return self.name
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
