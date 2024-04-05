e982c5bff751b00b551c251343f45ada535f764d
saravanankesa <140973037+saravanankesa@users.noreply.github.com>
2024-04-04 22:33:12 -0600

    Initial setup of html & css files

--- wk8fnl/capstone/monemome/models.py
+++ wk8fnl/capstone/monemome/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 TRANSACTION_TYPES = [
@@ -5,8 +6,22 @@ TRANSACTION_TYPES = [
     ('Income', 'Income')
 ]
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+
+    def __str__(self):
+        return self.name
+
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
 class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -17,20 +32,3 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"{self.type}: {self.name} - ${self.amount}"
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
-
-    def __str__(self):
-        return self.name
-
-
-class Account(models.Model):
-    name = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
-
--- wk8fnl/capstone/monemome/views.py
+++ wk8fnl/capstone/monemome/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username=username, password=password)
+        login(request, user)
+        return redirect('index')
+    return render(request, 'registration/register.html')
+
+@login_required
+def transaction_list(request):
+    transactions = Transaction.objects.filter(user=request.user)
+    return render(request, 'transactions/list.html', {'transactions': transactions})
